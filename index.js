96ce2770b2f2395aa097c61f81d54242af27a01c
whitexudan15 <tresortakoudawhitexudan15@gmail.com>
2025-05-28 11:10:46 +0000

    connexion à MongoDB et création de Schema et modèle

--- index.js
+++ index.js
@@ -4,6 +4,53 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
+// Connexion à la base de données
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connecté à MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error de connexion à MongoDB : ", error);
+  });
+
+// Définition du schema user
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+// Modèle User
+const User = mongoose.model("User", userSchema);
+
+// Définition du schema exercise
+const exerciseSchema = mongoose.Schema({
+  username: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    maxlength: 20,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+});
+// Modèle Exercise
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+
 app.use(cors());
 app.use(express.urlencoded({ extended: true })); // Permettre de parser les données provenant des formulaires classique <form>.
 app.use(express.json()); // Permettre de parser les données envoyés en POST avec du JSON brut dans le body
